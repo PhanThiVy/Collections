2a9c37497bcd3bc3442d3f27f16e776e24b0bf83
PhanThiVy <97930179+PhanThiVy@users.noreply.github.com>
2022-12-22 13:29:56 +0700

    update

--- src/arraylistdemo/LinkedListDemo.java
+++ src/arraylistdemo/LinkedListDemo.java
@@ -13,6 +13,7 @@ import java.util.List;
  * @author phanthivy
  */
 public class LinkedListDemo {
+
     public static void main(String[] args) {
         LinkedList<Integer> li = new LinkedList<>();
         li.add(2);
@@ -20,51 +21,48 @@ public class LinkedListDemo {
         li.add(4);
         li.add(11);
         li.add(3);
+        li.add(1, 0);
         List<Integer> li2 = new LinkedList<>();
-        li.add(6);
-        li.add(9);
-        li.add(8);
-        
+        li2.add(6);
+        li2.add(9);
+        li2.add(8);
         li.addAll(li2);
+        
         System.out.println("LinkedList: ");
         for (Integer l : li) {
-            System.out.print(l+ " ");
+            System.out.print(l + " ");
         }
-        
+
         //add first pos
         li.addFirst(0);
         System.out.println("");
         System.out.println("After add '0' at first position: ");
         for (Integer l : li) {
-            System.out.print(l+ " ");
+            System.out.print(l + " ");
         }
-        
+
         //add last pos
         li.addLast(0);
         System.out.println("");
         System.out.println("After add '0' at last position: ");
         for (Integer l : li) {
-            System.out.print(l+ " ");
+            System.out.print(l + " ");
         }
-        
+
         //remove first
         li.removeFirst();
         System.out.println("");
         System.out.println("After removeFirst(): ");
         for (Integer l : li) {
-            System.out.print(l+ " ");
+            System.out.print(l + " ");
         }
-        
+
         li.removeLast();
         System.out.println("");
         System.out.println("After removeLast(): ");
         for (Integer l : li) {
-            System.out.print(l+ " ");
+            System.out.print(l + " ");
         }
-        
-        
-        
-        
-        
+
     }
 }
